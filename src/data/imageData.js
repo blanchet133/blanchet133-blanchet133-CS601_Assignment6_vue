2cb99cddb924caf30b50dfe4b749b804ccb92e45
mblanchet1-admin <blanchet.michael1@gmail.com>
2022-07-31 22:47:20 -0400

    Added images... changing logic for multiple pics

--- src/data/imageData.js
+++ src/data/imageData.js
@@ -3,10 +3,22 @@ const images = require.context('./images', true, /\.jpg$/);
 const pic1 = require( "./images/dog.jpg");
 
 const imageData = {
-  imageList: [
+  dogList: [
     { id: 1, src: pic1 },
     { id: 2, src: images("./puppy.jpg") }
-  ]
+  ],
+  townList: [
+    { id: 1, src: images("./townNow.jpg")},
+    { id: 2, src: images("./townHistorical.jpg") }
+  ],
+  damList: [
+    { id: 1, src: images("./damFinished.jpg")},
+    { id: 2, src: images("./damConstruction.jpg") }
+  ],
+  butterflyList: [
+    { id: 1, src: images("./butterfly.jpg")},
+    { id: 2, src: images("./Caterpillar.jpg") }
+  ],
 }
 
 export default imageData;
